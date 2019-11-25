f7bd38a7e3e8c3bc15390bef7f6ccd1a649db5e7
satvik.dandale@gmail.com <satvik.dandale@gmail.com>
2019-11-25 14:55:37 +0530

    ""

---
+++ TY_PSUO/LAB/LAB_9_BookRecordFile.cpp
@@ -0,0 +1,13 @@
+/*
+    	Write a program to extend Book class:
+            1. To get book details from user
+            2. Store data to a file.
+        Write functions to store data to file and get data from file
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+}
