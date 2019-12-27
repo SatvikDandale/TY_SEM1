0e510c5cf62e7e6671aa7764281530753c2382b2
SatvikDandale <satvik.dandale@gmail.com>
2019-12-27 13:36:59 +0530

    Redudant

--- TY_DAA/LAB_4_Majority_Element.cpp
+++ TY_DAA/LAB_4_Majority_Element.cpp
@@ -1,126 +1,39 @@
-/*
+#include <bits/stdc++.h> 
+using namespace std; 
 
-	Use Divide and Conquor:
-	Two functions:
-		1. split(int *array, intl, int h)
-			split the array.
-			take occurances 
-			return majority element of current subarray
-		2. ret_max
-			take the majority element of two arrays
-			return the element whos occurence is more
+void findMajority(int arr[], int n) 
+{ 
+	int maxCount = 0; 
+	int index = -1;
+	for(int i = 0; i < n; i++) 
+	{ 
+		int count = 0; 
+		for(int j = 0; j < n; j++) 
+		{ 
+			if(arr[i] == arr[j]) 
+			count++; 
+		} 
+		
+		if(count > maxCount) 
+		{ 
+			maxCount = count; 
+			index = i; 
+		} 
+	} 
+	
+	if (maxCount > n/2) 
+	cout << arr[index] << endl; 
+	
+	else
+		cout << "No Majority Element" << endl; 
+} 
 
-*/
+int main() 
+{ 
+	int arr[] = {1, 1, 2, 1, 3, 5, 1}; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	
+	findMajority(arr, n); 
 
-#include<bits/stdc++.h>
-using namespace std;
-
-class majority_element{
-	private:
-		int *main_array;
-		int length;
-	public:
-		void get_input(){
-			cout<<"Enter the no of elments\n";
-			cin>>length;
-			cout<<"Enter the array:\n";
-			main_array = new int[length];
-			for(int i=0; i<length; i++){
-				cout<<"Enter element no: "<<i+1<<":\n";
-				cin>>main_array[i];
-			}
-		}
-		majority_element(){
-			get_input();
-		};
-
-		int ret_max(int *left, int *right){
-			/*
-				We want return the reference to the majority element whose occrance is more
-				1: left subarray
-				2: right subarray
-			*/
-			if (left[1] > right[1])
-				return 1;
-			return 2;
-		}
-
-		int* split(int *array, int l, int h){
-			// Check for single element first
-			if (l == h){
-				int *temp = new int[2];
-				temp[0] = array[l];
-				temp[1] = 1;
-				return temp;
-			}
-			else{
-				int m = (l+h)/2;
-				int *left, *right;
-				left = split(array, l, m);
-				right = split(array, m+1, h);
-				int major = ret_max(left, right);
-				/*
-					If we get 1:
-						Search left[0] in current array from m+1 to h
-						Add left[1] to it.
-					Else
-						Search right[0] in current array from l to m
-						Add right[1] to it.
-				*/
-				int *temp = new int[2];
-				int new_occurs = 0;
-				if (major == 1){
-					// We already have the occurance in left subarray
-					// Calculate in right subarray
-					for(int i=m+1; i<=h; i++)
-						if (array[i] == left[0])
-							new_occurs++;
-					new_occurs += left[1];
-					//While returning, we have to return majot element from left array
-					temp[0] = left[0];
-				}
-				else{
-					// We already /have the occurance in right subarray
-					// Calculate in left subarray
-					for(int i=0; i<m; i++)
-						if(array[i] == right[0])
-							new_occurs++;
-					new_occurs += right[1];
-					//While returning, we have to return majot element from right array
-					temp[0] = right[0];
-				}
-				// Now check if the new_occurs >= current_len/2
-				int len = h-l+1;
-				//LEFT
-				cout<<"\nLEFT:\n";
-				for(int i=l; i<=m; i++)
-					cout<<array[i];
-				//RIGHT
-				cout<<"\nRIGHT:\n";
-				for(int i=m+1; i<=h; i++)
-					cout<<array[i];
-				cout<<"\n";
-				if(new_occurs >= len/2){
-					if(major == 1)
-						temp[1] = new_occurs;
-					else
-						temp[1] = new_occurs;
-					cout<<"O: "<<new_occurs<<endl;
-					cout<<"L: "<<left[0];
-					cout<<" R: "<<right[0]<<endl;
-					return temp;
-				}
-			}
-		}
-		void split_utility(){
-			int *final_majority_element = split(main_array, 0, length-1);
-			cout<<"The majority element is: "<<final_majority_element[0]<<"\nAnd the no of occurances are: "<<final_majority_element[1]<<endl;
-		}
-
-};
-
-int main(){
-	majority_element *m = new majority_element();
-	m->split_utility();
-
-}
+	return 0; 
+} 
--- TY_PSUO/LAB/LAB_9_BookRecordFile.cpp
+++ TY_PSUO/LAB/LAB_9_BookRecordFile.cpp
@@ -7,7 +7,239 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+#define id_width 6
+#define name_width 20
+#define author_width 20
+#define price_width 10
+
+static char FILENAME[] = "books.dat";
+
+class Book{
+    private:
+        int id;
+        float price;
+        char name[20], author[20];
+    public:
+        Book(){
+            id = 0;
+            price = 0;
+            strcpy(name, "");
+            strcpy(author, "");
+        }
+        Book(int id, float price, char* name, char* author){
+            // Paremeterized contructor
+            this->id = id;
+            this->price = price;
+            strcpy(this->name, name);
+            strcpy(this->author, author);
+        }
+        void setDetails(){
+            cout<<"\nEnter the Book Id: ";
+            cin>>this->id;
+            cout<<"Enter the Price of this book: ";
+            cin>>this->price;
+            cin.ignore();   // Ignore the garbage remaining in the istream.
+
+            cout<<"Enter the Name of the book: ";
+            cin.getline(this->name, 20);
+            cout<<"Enter the name of the Author of this book: ";
+            cin.getline(this->author, 20);
+        }
+        // GETTERS
+        int getId(){return this->id;}
+        float getPrice(){return this->price;}
+        char* getName(){return this->name;}
+        char* getAuthor(){return this->author;}
+
+        // SETTERS
+        void setId(int id){
+            this->id = id;
+        }
+        void setPrice(int price){
+            this->price = price;
+        }
+
+        void setName(char* name){
+            strcpy(this->name, name);
+        }
+        void setAuthor(char* author){
+            strcpy(this->author, author);
+        }
+
+        void putDetails(){
+            cout<<setw(id_width)<<this->id;
+            cout<<setw(name_width)<<this->name;
+            cout<<setw(author_width)<<this->author;
+            cout<<setw(price_width)<<this->price<<endl;
+        }
+        friend float getTotal(Book *, int);
+
+
+};
+
+class BookRecords: public Book{
+        public:
+                void initialiseFile();
+                void displayFile(fstream &fileObject);
+                void displayUtility(ostream &outputStream, Book book);
+                void newRecord(fstream &fileObject);
+
+                // Constructor
+                BookRecords(){
+                    initialiseFile();
+                }
+};
+
+float getTotal(Book *list, int count){
+    // Iterate through the list and calculate the total price of books.
+    int total = 0;
+    for(int i=0; i<count; i++)
+        total += list[i].price;
+    return total;
+}
+//////////////////////////////////////////////////
+///////////////////////MAIN///////////////////////
 
 int main(){
     
+    BookRecords *record = new BookRecords();
+    // Here file will be initialised
+
+    // File Object
+    fstream fileObject(FILENAME, ios::in | ios::out);
+
+    int choice;
+    while(1){
+        cout<<"\n1. Display the current Book Record\n"
+            <<"2. Add new Book Record\n"
+            <<"3. Exit\n";
+        cin>>choice;
+        switch(choice){
+            case 1:
+                record->displayFile(fileObject);
+                break;
+            case 2:
+                record->newRecord(fileObject);
+                break;
+            case 3:
+                exit(1);
+            default:
+                cout<<"Enter a valid choice\n";
+        }
+    }
+
 }
+//////////////////////END OF MAIN/////////////////
+//////////////////////////////////////////////////
+
+void BookRecords::initialiseFile(){
+        fstream File;
+        File.open(FILENAME, ios::in);
+
+        // Check if the file exisis.
+        if(!File){
+                // It does not exists.
+                // Open in OUT mode
+                File.close();
+                File.open(FILENAME, ios::out | ios::binary);
+
+                // Make 10 Default book entries
+                Book *b1 = new Book();
+
+                for(int i=0; i < 10; i++)
+                    File.write((char*)b1, sizeof(*b1));
+                File.close();
+                cout<<"NEW FILE CREATED\n";
+        }
+        else{
+            // The file exists
+            File.close();
+            cout<<"FILE ALREADY EXISTED\n";
+        }
+}
+
+void BookRecords::displayUtility(ostream &outputStream, Book book){
+    outputStream<<setw(id_width)<<book.getId();
+    outputStream<<setw(name_width)<<book.getName();
+    outputStream<<setw(author_width)<<book.getAuthor();
+    outputStream<<setw(price_width)<<book.getPrice()<<endl;
+}
+
+void BookRecords::displayFile(fstream &fileobject){
+    ofstream outPrintFile("print.txt");
+    
+    if(!outPrintFile){
+        cout<< "File could not be opened." <<endl;
+        exit(1);
+    }
+
+    cout<<setw(id_width)<<"BookId";
+    cout<<setw(name_width)<<"Book Name";
+    cout<<setw(author_width)<<"Book Author";
+    cout<<setw(price_width)<<"Book Price"<<endl;
+
+    outPrintFile<<setw(id_width)<<"BookId";
+    outPrintFile<<setw(name_width)<<"Book Name";
+    outPrintFile<<setw(author_width)<<"Book Author";
+    outPrintFile<<setw(price_width)<<"Book Price"<<endl;
+
+    fileobject.seekg(0, ios::beg);
+
+    Book *b = new Book();
+
+    while(!fileobject.eof()){
+        fileobject.read((char*)b, sizeof(*b));
+
+        this->displayUtility(outPrintFile, *b);
+        this->displayUtility(cout, *b);
+    }
+}
+
+void BookRecords::newRecord(fstream &fileObject){
+    char bookName[20];
+    char authorName[20];
+    int bookId;
+    int price;
+    Book *b = new Book();
+
+    cout<<"Enter the new book ID (1-10):\n";
+    cin>>bookId;
+
+    fileObject.seekg((bookId - 1) * sizeof(*b));    // Get there
+
+    // Read the current status.
+    fileObject.read((char*)b, sizeof(*b));
+    
+    // If the book id is 0, only then we can insert new book
+    if (b->getId() == 0){
+        // We can add new item
+        b->setId(bookId);
+
+        cout<<"Enter the Book name:\n";
+        cin.ignore();
+        cin.getline(bookName, 20);
+        b->setName(bookName);
+
+        cout<<"Enter the Author name:\n";
+        cin.getline(authorName, 20);
+        b->setAuthor(authorName);
+
+        cout<<"Enter the price of this book.\n";
+        cin>>price;
+        b->setPrice(price);
+
+
+        // Seek the write pointer
+        fileObject.seekp((bookId - 1) * sizeof(*b));
+        fileObject.write((char*)b, sizeof(*b));
+
+    }
+    else{
+        cout<<"Book No: "<<bookId
+            << " already exists"<<endl;
+    }
+    
+}
+
+
+
